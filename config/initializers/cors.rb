5576e133b2c45ddcf8ace628341ed19dafd95ab0
zunerz671 <zunerz671@gmail.com>
2025-12-10 21:13:59 +1000

    Frontend capable

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173", "https://guam-math.netlify.app"
 
     resource "*",
       headers: :any,
